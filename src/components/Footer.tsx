debdf52ad6480e6597c0487b76f7a21564cd2908
Josh Daniel <joshpersonal8@gmail.com>
2021-08-20 11:43:23 +0800

    Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,14 @@ import React from "react";
 export const Footer: React.FC = () => {
   return (
     <footer className='flex dmd:flex-col dlg:flex-col justify-between px-32 dmd:px-4 dmd:items-center dlg:items-center dmd:bg-transparent w-full absolute bottom-0 py-5 bg-green-900 dmd:text-xs dlg:text-base'>
-      <p>© Josh Daniel Bañares 2021 • All Rights Reserved</p>
+      <p>© Josh Daniel 2021 • All Rights Reserved</p>
       <div className='dmd:mt-3'>
-        <a className='mr-16 link' href='https://github.com/joshxfi/gh-stats-generator' target='_blank' rel='noreferrer'>
+        <a
+          className='mr-16 link'
+          href='https://github.com/joshxfi/gh-stats-generator'
+          target='_blank'
+          rel='noreferrer'
+        >
           GitHub
         </a>
         <a
@@ -16,7 +21,12 @@ export const Footer: React.FC = () => {
         >
           Generator
         </a>
-        <a className='link' href='https://xfi.vercel.app' target='_blank' rel='noreferrer'>
+        <a
+          className='link'
+          href='https://xfi.vercel.app'
+          target='_blank'
+          rel='noreferrer'
+        >
           Portfolio
         </a>
       </div>
