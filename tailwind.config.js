a24e0691c1df7b57d5fcbcb1247e709dc3b1fc6e
Josh Daniel <joshpersonal8@gmail.com>
2022-02-12 11:10:13 +0800

    chore: update tailwindcss to latest

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,7 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./index.html", "./src/**/*.tsx"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./index.html", "./src/**/*.tsx"],
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {
-      screens: {
-        dmd: { max: "820px" },
-        dlg: { max: "1024px" },
-        dxl: { max: "1464px" },
-      },
-    },
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
