f0cb35d16ceb4251796e233a8eb2d1e8564c43e6
hyamero <daleban.dev@gmail.com>
2024-05-08 09:57:48 +0800

    feat: form with validation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { GhStatsForm } from "@/components/gh-stats-form";
+
 export default function Home() {
   return (
     <main className="min-h-screen grid place-items-center">
-      <div className="text-center">
-        <h1 className="text-5xl font-bold">GitHub Stats Generator</h1>
-        <p>by Omsimos</p>
+      <div className="space-y-10">
+        <div className="text-center">
+          <h1 className="text-5xl font-bold">GitHub Stats Generator</h1>
+          <p>by Omsimos</p>
+        </div>
+        <GhStatsForm />
       </div>
     </main>
   );
---
+++ src/components/gh-stats-form.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input";
+import { toast } from "sonner";
+
+const FormSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
+
+export function GhStatsForm() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast("You submitted the following values:", {
+      description: JSON.stringify(data, null, 2),
+    });
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-full max-w-md space-y-6"
+      >
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>GitHub Account</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
