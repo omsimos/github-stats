486e0dd83d33d57b90f256ae3b4ea6f0efe8c9b1
hyamero <daleban.dev@gmail.com>
2024-05-08 19:28:40 +0800

    chore: add images hostnames in config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "github-readme-stats.vercel.app",
+      },
+      {
+        protocol: "https",
+        hostname: "github-readme-streak-stats.herokuapp.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
