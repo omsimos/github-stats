c8e8c55edffc86809ea527c50bf3cf700107de74
hyamero <daleban.dev@gmail.com>
2024-05-10 12:29:38 +0800

    chore: footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="absolute left-0 bottom-0 w-full font-mono text-sm text-center py-3">
+      <p className="text-center text-muted-foreground">
+        &copy; 2024 OMSIMOS · MIT License
+      </p>
+    </footer>
+  );
+}
