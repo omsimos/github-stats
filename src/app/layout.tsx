ea5b061b1b9c61fbc614c397b48ce92826dd73f6
hyamero <daleban.dev@gmail.com>
2024-05-10 12:30:08 +0800

    chore: improve meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,31 @@
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import NextTopLoader from "nextjs-toploader";
 import Navbar from "@/components/navbar";
 import "./globals.css";
+import Footer from "@/components/footer";
 
 export const metadata: Metadata = {
-  title: "GH Stats Gen | Omsimos",
-  description: "Generate your GitHub stats for your README with ease!",
+  metadataBase: new URL("https://www.github-stats.omsimos.com"),
+  title: "GitHub Stats Generator | Omsimos",
+  description:
+    "Make your special repository feel special! Generate GitHub README stats with ease! Generate your stats, streaks, & top languages with 40+ themes to choose from! ",
+  robots: "index, follow",
+  openGraph: {
+    type: "website",
+    title: "Frameyu — Supercharge Your Campaign!",
+    description:
+      "Effortlessly manage and publish custom frames for your marketing campaigns with Frameyu, the free online frame publisher. Create shareable links to boost engagement. Download in high-quality images, watermark-free. Get started for free today!",
+    images: [
+      "https://github.com/omsimos/github-stats/assets/78056869/ded4ae46-524a-433b-bb54-c7e2c311e5da",
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+  },
 };
 
 export default function RootLayout({
@@ -18,12 +35,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={GeistSans.className}>
+      <body className={`${GeistSans.className} ${GeistMono.variable}`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Navbar />
           <Toaster />
           <NextTopLoader />
           {children}
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
