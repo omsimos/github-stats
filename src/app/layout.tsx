8ea3eb765655d5afb3982407dbfd01218fa70812
Josh Daniel Bañares <joshxfi.dev@gmail.com>
2025-06-13 20:48:56 +0800

    fix: suppress hydration warning for themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${GeistSans.className} ${GeistMono.variable}`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Toaster />
