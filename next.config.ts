1452b0add7474d3934e58744cfd8c689b06bc5f8
Josh Daniel Bañares <joshxfi.dev@gmail.com>
2025-06-13 21:29:34 +0800

    chore: update eslint to v9

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
     remotePatterns: [
